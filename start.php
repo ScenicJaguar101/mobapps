ca740b6e3463dc577b0753b4a637804ca5472dce
Jackson <scenicjaguar101@gmail.com>
2018-03-23 13:50:47 -0400

    Update start.php

--- start.php
+++ start.php
@@ -1,13 +1,8 @@
 <html>
-
 <head>
-
 </head>
-
 <body>
-
 <form method="POST" action="receive.php">
-
 <table cols="2">
 <tr><td align="right">Textbox:</td><td align="left"><input name="textbox" type="text" value="Default Text"/></td></tr>
 <tr><td align="right">Textarea:</td><td align="left"><textarea name="textarea" rows="10" cols="40">Default Text</textarea></td></tr>
@@ -16,12 +11,8 @@
 <option value="A">A</option>
 <option value="cookie">cookie</option>
 </select></td></tr>
-
 <tr><td align="right">Submit button:</td><td align="left"><input type="submit" value="Button Title"/></td></tr>
 </table>
-
 </form>
-
 </body>
-
 </html>
