f357a6ce6d9a5684089883dee82f7a113b9a91e2
Jackson <scenicjaguar101@gmail.com>
2018-04-02 10:47:59 -0400

    Update receive.php

--- receive.php
+++ receive.php
@@ -7,28 +7,24 @@
 <body>
 
 <?php
-
 function receivePOST($variableName)
 {
  return htmlspecialchars($_POST[$variableName]);
 }
-
 $textBox = receivePOST("textbox");
-$textArea = receivePOST("textarea");
-$dropDown = receivePOST("dropdown");
-
-if (!strlen($textBox) || !strlen($textArea) || !strlen($dropDown))
-{
- echo "ERROR: Not all expected values have been entered/submitted!";
-}
-else
-{
+//$textArea = receivePOST("textarea");
+//$dropDown = receivePOST("dropdown");
+//if (!strlen($textBox) || !strlen($textArea) || !strlen($dropDown))
+//{
+// echo "ERROR: Not all expected values have been entered/submitted!";
+//}
+//else
+//{
  echo "Received submitted data!<br/><br/>";
  echo "Text box contents: $textBox<br/><br/>";
- echo "Text area contents: $textArea<br/><br/>";
- echo "Drop down contents: $dropDown<br/><br/>";
-}
-
+ //echo "Text area contents: $textArea<br/><br/>";
+ //echo "Drop down contents: $dropDown<br/><br/>";
+//}
 ?>
 
 </body>
